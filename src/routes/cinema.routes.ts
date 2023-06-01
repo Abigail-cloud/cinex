000722b6fa9072babd2bb5bcf95955a0df8dd85e
Toluwalase Abigail <toluabby12@gmail.com>
2023-06-01 15:35:25 +0100

    Added Documentaton

--- src/routes/cinema.routes.ts
+++ src/routes/cinema.routes.ts
@@ -2,31 +2,10 @@ import { Router } from "express";
 import  {OpenAPI} from 'openapi-types';
 import { CinemaController } from "../controller/cinema.controller";
 
-
 const cinemaController = new CinemaController();
 const cinemaRouter = Router();
 
-
-/**
- * @openapi
- * /cinema/book:
- *   post:
- *     summary: Create booking Tickets for users
- *     description: Create Tickets based on the available seats
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/User'
- *     responses:
- *       '201':
- *         description: User created successfully
- *       '400':
- *         description: Invalid request data
- */
 cinemaRouter.post('/book', cinemaController.bookTickets.bind(cinemaController));
-
 
 
 export default cinemaRouter;
